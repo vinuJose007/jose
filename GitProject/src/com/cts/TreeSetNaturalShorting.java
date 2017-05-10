009d8c5fb6ec59f490c52dbdfe0220c007311b46
Vinu <Vinu@MyLaptop>
2017-05-11 03:37:49 +0530

    TreeSetNaturalShortingOrder

---
+++ GitProject/src/com/cts/TreeSetNaturalShorting.java
@@ -0,0 +1,23 @@
+package com.cts;
+
+import java.util.TreeSet;
+
+public class TreeSetNaturalShorting {
+
+	public static void main(String[] args) {
+		//TreeSet<Integer> tr=new TreeSet<Integer>();
+		TreeSet<String> tr=new TreeSet<String>();
+		
+//		tr.add(5);
+//		tr.add(3);
+//		tr.add(8);
+		
+		tr.add("Avanu");
+		tr.add("ajose");
+		tr.addAll(null);
+		
+		System.out.println(tr);
+		
+	}
+
+}
